beeb597ce376161880d4ff84dab681b183b93f56
Jeshua IS <jeshuairiarte73@gmail.com>
2026-06-11 18:53:57 -0500

    Update sheetdb.js

--- js/sheetdb.js
+++ js/sheetdb.js
@@ -26,8 +26,6 @@ async function cargarMenuHamburguesas() {
 
       // Esto borra cualquier "S/." que venga del Excel y le pone un formato limpio y uniforme
       let precioLimpio = burger.precio.toString().replace(/S\/\.?\s?/g, '');
-      // Opción A: Si tu base de datos devuelve SOLO el número (ej: 15.50)
-      <p class="menu-card__price">S/. ${parseFloat(primeraBurger.precio).toFixed(2)}</p>
       
       htmlContenido += `
         <article class="menu-card menu-card--hero" data-price="${primeraBurger.precio}" data-name="${primeraBurger.nombre}">
